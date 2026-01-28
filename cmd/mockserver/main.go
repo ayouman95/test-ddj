bcfde8dd30c6bfd1872bac261487ddc105fbfd23
ayouman <zhuhaihao@pandomobi.com>
2026-01-29 00:39:20 +0800

    update

--- cmd/mockserver/main.go
+++ cmd/mockserver/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"math/rand"
 	"net/http"
@@ -8,10 +9,20 @@ import (
 )
 
 func main() {
+	var sleepFlag time.Duration
+	flag.DurationVar(&sleepFlag, "sleep", -1, "Fixed sleep duration. Set to 0 for stress test. Default -1 (random 100-1000ms)")
+	flag.Parse()
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// Random sleep between 100ms and 1000ms
-		sleepDuration := time.Duration(100+rand.Intn(901)) * time.Millisecond
-		time.Sleep(sleepDuration)
+		if sleepFlag >= 0 {
+			if sleepFlag > 0 {
+				time.Sleep(sleepFlag)
+			}
+		} else {
+			// Random sleep between 100ms and 1000ms
+			sleepDuration := time.Duration(100+rand.Intn(901)) * time.Millisecond
+			time.Sleep(sleepDuration)
+		}
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})
